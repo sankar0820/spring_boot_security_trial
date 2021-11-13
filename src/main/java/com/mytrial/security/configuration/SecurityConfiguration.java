e4703c0e61094c994033b2af5527753d181a7803
Sankar <jaysankar.kakarlapudi@gmail.com>
2021-11-13 22:42:25 +0530

    intial commit

--- src/main/java/com/mytrial/security/configuration/SecurityConfiguration.java
+++ src/main/java/com/mytrial/security/configuration/SecurityConfiguration.java
@@ -1,11 +1,10 @@
 package com.mytrial.security.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -13,10 +12,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("sankar").password("naaishtam").roles("USER");
+		 PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		auth.inMemoryAuthentication().withUser("sankar").password(encoder.encode("naaishtam")).roles("USER");
 	}
-	@Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+	
 }
